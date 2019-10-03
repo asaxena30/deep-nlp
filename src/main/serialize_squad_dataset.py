8ad970f389f3ec67a3d33674367aa00579d5592e
Abhishek Saxena <asaxena@FinancialEngines.com>
2019-10-03 00:25:33 -0700

    ignored local files

--- src/main/serialize_squad_dataset.py
+++ src/main/serialize_squad_dataset.py
@@ -1,20 +1,24 @@
 from collections import namedtuple
-from typing import List, NamedTuple
+from typing import List, NamedTuple, Dict
 
 import time
 import pickle
 
+import nltk
+from nltk.tag.perceptron import PerceptronTagger
+
 from src.data.dataset.dataset import SquadDataset
 from src.data.dataset.datasetreaders import SquadReader
-from src.data.instance.instance import QAInstanceWithAnswerSpan
-from torch.utils.data.dataloader import DataLoader
+from src.data.instance.instance import QAInstanceWithAnswerSpan, TaggedQAInstanceWithAnswerSpan
 from nltk.tokenize import word_tokenize
+import torch
 
+from src.util import vector_encoding_utils
 
 dataset_data_file_path: str = "../../data/SQuAD"
 
-# training_data_file_path: str = dataset_data_file_path + "/train-v2.0.json"
-dev_data_file_path: str = dataset_data_file_path + "/dev-v2.0.json"
+training_data_file_path: str = dataset_data_file_path + "/train-v2.0.json"
+# dev_data_file_path: str = dataset_data_file_path + "/dev-v2.0.json"
 
 # training_data_file_path: str = dataset_data_file_path + "/sample.json"
 # dev_data_file_path: str = dataset_data_file_path + "/sample.json"
@@ -33,10 +37,19 @@ SquadTuple: NamedTuple = namedtuple('SquadTuple', ['question_tokens', 'passage_t
 
 BATCH_SIZE: int = 10
 
+universal_pos_tagset = ['ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM', 'PRON', 'PRT', 'VERB', '.', 'X', 'UNK']
+null_tag_tensor = torch.zeros((1, 1, len(universal_pos_tagset)))
+
+pos_tags_to_tensor_dict: Dict[str, torch.Tensor] = vector_encoding_utils.build_one_hot_encoded_tensors_for_tags(universal_pos_tagset)
+
+UNKNOWN_TAG = "UNK"
+
+tagger = PerceptronTagger()
+
 
 def collate_with_padding(batch):
     if not isinstance(batch[0], QAInstanceWithAnswerSpan):
-        raise NotImplementedError("only a QAInstanceWithAnswerSpan is supported for this class")
+        raise NotImplementedError("only a QAInstanceWithAnswerSpan/subclass is supported")
 
     max_question_length_instance = max(batch, key = lambda batch_item: len(batch_item.question))
     max_question_length = len(max_question_length_instance.question)
@@ -50,14 +63,16 @@ def collate_with_padding(batch):
         question_padding_size = max_question_length - instance_question_length
         instance.question = instance.question + ['pad'] * question_padding_size
 
-        instance_passage_length = len(instance.passage)
-        instance_padding_size = max_passage_length - instance_passage_length
-        instance.passage = instance.passage + ['pad'] * instance_padding_size
+        if instance.question_pos_tags is not None:
+            instance.question_pos_tags = torch.cat(instance.question_pos_tags + [null_tag_tensor] * question_padding_size, dim = 1)
 
-        # batch_as_tuples.append(
-        #     SquadTuple(instance.question, instance.passage, instance.answer_start_and_end_index[0],
-        #                instance.answer_start_and_end_index[1],
-        #                instance.answer))
+        instance_passage_length = len(instance.passage)
+        passage_padding_size = max_passage_length - instance_passage_length
+        instance.passage = instance.passage + ['pad'] * passage_padding_size
+
+        if instance.passage_pos_tags is not None:
+            instance.passage_pos_tags = torch.cat(instance.passage_pos_tags + [null_tag_tensor] * passage_padding_size, dim = 1)
+
         batches.append(instance)
     return batches
 
@@ -93,6 +108,11 @@ def get_squad_dataset_from_file(file_path: str) -> SquadDataset:
                          passage_tokens[answer_start_marker_index + 1: answer_end_marker_index] + \
                          passage_tokens[answer_end_marker_index + 1:]
 
+        pos_tags_for_passage_tokens = [pos_tags_to_tensor_dict[tagged_word[1]].view(1, 1, -1) for tagged_word in
+                                       nltk.tag._pos_tag(passage_tokens, tagset = 'universal', tagger = tagger, lang = 'eng')]
+        pos_tags_for_question_tokens = [pos_tags_to_tensor_dict[tagged_word[1]].view(1, 1, -1) for tagged_word in
+                                        nltk.tag._pos_tag(question_tokens, tagset = 'universal', tagger = tagger, lang = 'eng')]
+
         answer_span_start_token_index: int = answer_start_marker_index
 
         # removing the start marker, shifts the answer towards the start by an additional index,
@@ -101,37 +121,54 @@ def get_squad_dataset_from_file(file_path: str) -> SquadDataset:
 
         answer_indices = (answer_span_start_token_index, answer_span_end_token_index)
 
-        instance = QAInstanceWithAnswerSpan(question = question_tokens, passage = passage_tokens,
+        instance = TaggedQAInstanceWithAnswerSpan(question = question_tokens, question_pos_tags = pos_tags_for_question_tokens,
+                                            passage = passage_tokens, passage_pos_tags = pos_tags_for_passage_tokens,
                                             answer = squad_qa_instance_as_dict['answer'],
                                             answer_start_and_end_index = answer_indices,
                                             total_length = len(question_tokens + passage_tokens))
 
         squad_dataset_list.append(instance)
 
+        # print(passage_tokens)
+        # print(question_tokens)
+        # print(squad_qa_instance_as_dict['answer'])
+
     return SquadDataset(sorted(squad_dataset_list, key = lambda x: x.total_length))
 
 
 print("loading dataset...., time = " + str(time.time()))
 # train_dataset = get_squad_dataset_from_file(training_data_file_path)
-# dataset = get_squad_dataset_from_file(dev_data_file_path)
-# with open("./squad_dataset_dev", "wb+") as f:
-#     pickle.dump(dataset, f)
 
-dataset = None
-with open("../../data/squad_serialized/squad_dataset_dev", "rb") as f:
-    dataset = pickle.load(f)
-
-print("dataset loaded...., time = " + str(time.time()))
-
-dataloader = DataLoader(dataset, batch_size = BATCH_SIZE, collate_fn = collate_with_padding, shuffle = True)
-
-print("dataloader built...., time = " + str(time.time()))
+dataset = get_squad_dataset_from_file(training_data_file_path)
 
 iteration_count: int = 0
 
-for batch in dataloader:
-    if iteration_count == 1:
-        print(batch)
+for instance in dataset:
+    if iteration_count % 10000 == 0:
+        print(instance.passage)
+        print(instance.question)
+    iteration_count += 1
+
+
+print("serializing and re-loading dataset")
+
+with open("./squad_dataset_serialized", "wb+") as f:
+    pickle.dump(dataset, f)
+
+
+with open("./squad_dataset_serialized", "rb") as f:
+    serialized_dataset = pickle.load(f)
+
+# print("dataset loaded...., time = " + str(time.time()))
+
+iteration_count = 0
+
+for instance in serialized_dataset:
+    if iteration_count % 10000 == 0:
+        print(instance.passage)
+        print(instance.passage_pos_tags)
+        print(instance.question)
+        print(instance.question_pos_tags)
     iteration_count += 1
 
 print(iteration_count)
